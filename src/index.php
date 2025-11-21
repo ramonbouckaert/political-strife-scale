89227d48daf3e33f2364b96d6a873af23b4823f1
Ramon Bouckaert <ramon@bouckaert.com.au>
2025-11-21 21:28:53 +1100

    Ui improvements.

--- src/index.php
+++ src/index.php
@@ -12,42 +12,53 @@ $value = strife_get_value_from_request();
     <style>
         html, body {
             height: 100%;
-	    margin: 0;
+	        margin: 0;
             font-family: Arial Narrow, Arial, sans-serif; 
         }
         body, .scale {
             display: flex;
-	    align-items: center;
+	        align-items: center;
             flex-direction: column;
-	}
+	    }
         body {
             justify-content: space-around;
-	}
+	    }
         .scale {
             width: 200px;
         }
         img {
             max-width: 100%;
             height: auto;
-	}
+        }
         input[type="range"] {
             width: 200px;
             margin: 0;
         }
         .scale-labels {
             display: flex;
-	    justify-content: space-between;
-	    margin-top: 14px;
+	        justify-content: space-between;
             width: 200px;
-	}
+	    }
         .scale-labels span {
-	    display: inline-block;
-            font-size: 10px;
+	        display: inline-block;
+            font-size: 11px;
             writing-mode: vertical-lr;
-            transform: rotate(-45deg);
-            transform-origin: top center;
-	    white-space: nowrap;
+            transform: translate(-5.5px) rotate(-45deg);
+            transform-origin: top right;
+	        white-space: nowrap;
             width: 0;
+            flex-grow: 1;
+            align-content: end;
+        }
+        .wider {
+            flex-grow: 1.5 !important;
+        }
+        .scale-labels span a {
+            text-decoration: none;
+            color: inherit;
+        }
+        .footer {
+            font-size: 11px;
         }
     </style>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -77,28 +88,28 @@ $value = strife_get_value_from_request();
     >
     <div class="scale">
         <label for="temp">Generate a new image:</label><br />
-	<input type="range" id="new_position" name="new_position" list="markers" step="1" value="<?= $value ?>" />
+        <input type="range" id="new_position" name="new_position" list="markers" step="1" value="<?= $value ?>" />
 
         <datalist id="markers">
             <option value="0">Colourful</option>
-            <option value="14">Controversial</option>
-            <option value="29">Under Pressure</option>
-            <option value="43">Troubled</option>
-            <option value="57">Beleaguered</option>
-            <option value="71">Embattled</option>
-            <option value="86">Former</option>
+            <option value="12">Controversial</option>
+            <option value="24">Under Pressure</option>
+            <option value="35">Troubled</option>
+            <option value="47">Beleaguered</option>
+            <option value="65">Embattled</option>
+            <option value="82">Former</option>
             <option value="100">Disgraced</option>
-	</datalist>
+        </datalist>
 
         <div class="scale-labels">
-            <span>Colourful</span>
-            <span>Controversial</span>
-            <span>Under Pressure</span>
-            <span>Troubled</span>
-            <span>Beleaguered</span>
-            <span>Embattled</span>
-            <span>Former</span>
-            <span>Disgraced</span>
+            <span><a href="/colourful">Colourful</a></span>
+            <span><a href="/controversial">Controversial</a></span>
+            <span><a href="/underpressure">Under Pressure</a></span>
+            <span><a href="/troubled">Troubled</a></span>
+            <span class="wider"><a href="/beleaguered">Beleaguered</a></span>
+            <span class="wider"><a href="/embattled">Embattled</a></span>
+            <span class="wider"><a href="/former">Former</a></span>
+            <span class="wider"><a href="/disgraced">Disgraced</a></span>
         </div>
     </div>
 
